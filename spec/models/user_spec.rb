8c2b039f27cde9df18ac2c3e08513dbbb8c5a49c
Will <will.h.paul@gmail.com>
2013-05-26 18:35:10 -0400

    Add email uniqueness check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,13 +26,33 @@ describe User do
 		it {should_not be_valid}
 	end
 
+	describe "when name is too long" do
+		before {@user.name = "b" * 51}
+		it {should_not be_valid}
+	end
+
 	describe "when email is not present" do
 		before {@user.email = " "}
 		it {should_not be_valid}
 	end
 
-	describe "when name is too long" do
-		before {@user.name = "b" * 51}
+	describe "when the email is not valid" do
+		before {@user.email = "jj.com"}
+		it {should_not be_valid}
+	end
+
+	describe "when the email is valid" do
+		before {@user.email = "j@j.com"}
+		it {should be_valid}
+	end
+
+	describe "when email is already in use" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
 		it {should_not be_valid}
 	end
 end
